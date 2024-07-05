565d3d2737d8e4dda49e549286e489dc39499833
tomercarolla <tomerca@accessibe.com>
2024-07-05 23:51:19 +0300

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,8 +18,6 @@ export default function HomePage() {
   const hasNoTodos = filteredTodos.length === 0 && !isLoading;
   const hasMorePages = page < totalPage;
   
-  
-
   useEffect(() => {
     if (!initialLoadComplete.current) {
       get({ page })
